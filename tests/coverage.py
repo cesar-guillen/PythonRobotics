ec98977e89f8c410da40a4c9f1927f5980b09fa3
CesarGitHub <91685879+cesar-guillen@users.noreply.github.com>
2024-06-17 13:23:57 +0200

    Update and rename coverage.py to coverage_tracker.py

--- tests/coverage.py
+++
@@ -1,4 +0,0 @@
-branch_coverage = {
-    "normal_vector_esitmation_1": False,  # if branch for np is close
-    "normal_vector_esitmation_2": False  # else branch np is not close
-}
---
+++ tests/coverage_tracker.py
@@ -0,0 +1,6 @@
+branch_coverage = {
+    "normal_vector_esitmation_1": False,  # if branch for np is close
+    "normal_vector_esitmation_2": False,  # else branch np is not close
+    "check_occupied_from_xy_index_1" : False, # if branch if its occupied
+    "check_occupied_from_xy_index_2" : False # not occupied
+}
