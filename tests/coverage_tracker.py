25e37bdf41e40028eef522c9a4c7cfedde162fdb
CesarGitHub <91685879+cesar-guillen@users.noreply.github.com>
2024-06-23 20:32:47 +0200

    Update coverage_tracker.py

--- tests/coverage_tracker.py
+++ tests/coverage_tracker.py
@@ -1,6 +1,9 @@
 branch_coverage = {
     "normal_vector_esitmation_1": False,  # if branch for np is close
     "normal_vector_esitmation_2": False,  # else branch np is not close
+}
+
+branch_coverage_2 = {
     "verify_node_x_small": False, 
     "verify_node_y_small": False,
     "verify_node_x_large": False,
