f2a5c7ebed563319375ab56a207c20762d7a5ac8
d-campagne <121505924+d-campagne@users.noreply.github.com>
2024-06-27 12:34:38 +0200

    Add files via upload

---
+++ Mapping/lidar_to_grid_map/branch_coverage.py
@@ -0,0 +1,14 @@
+
+branch_coverage = {
+    "init_flood_fill_1": False,  
+    "init_flood_fill_2": False, 
+    "flood_fill_1": False,
+    "flood_fill_2": False, 
+    "flood_fill_3": False, 
+    "flood_fill_4": False,
+    "flood_fill_5": False,
+    "flood_fill_6": False,
+    "flood_fill_7": False,
+    "flood_fill_8": False,
+    "flood_fill_9": False
+}
